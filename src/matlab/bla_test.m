bb038cb7c911994274000fa0d9f9619564782f19
Haoyang Liu <liuhaoyang@pku.edu.cn>
2021-01-16 15:41:02 +0800

    add matlab

---
+++ src/matlab/bla_test.m
@@ -0,0 +1,34 @@
+function bla_test(n)
+
+if nargin < 1
+    n = 100;
+end
+
+% random matrices
+A = rand(n);
+B = rand(n);
+b = rand(n, 1);
+
+% for gemv
+tic;
+for i = 1:50
+    c = A * b;
+end
+toc;
+
+for i = 1:50
+    c = A' * b;
+end
+toc;
+
+% for gemm
+tic;
+for i = 1:5
+    C = A * B;
+end
+toc;
+
+for i = 1:5
+    C = A' * B;
+end
+toc;
